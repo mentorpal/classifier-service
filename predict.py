9ee3cee82ad1217286d268a3bda10fbfbd5cff4b
Milan Simonovic <mbsimonovic@gmail.com>
2022-02-11 06:47:29 +0100

    answer question

---
+++ predict.py
@@ -0,0 +1,42 @@
+import json
+import os
+from module import ClassifierFactory
+
+def handler(event, context):
+    mentor = '6109d2a86e6fa01e5bf3219f'
+    shared = os.environ.get('SHARED_ROOT')
+    question = 'how should we call you?'
+    os.environ['CLASSIFIER_ARCH'] = 'module.arch.lr_transformer'
+    result = (
+            ClassifierFactory()
+            .new_prediction(
+                mentor=mentor, shared_root=shared, data_path='models'
+            )
+            .evaluate(question=question,shared_root=shared)
+        )
+    body = {
+        "question": question,
+        "answer_id": result.answer_id,
+        "answer_text": result.answer_text,
+        "answer_media": result.answer_media,
+        "confidence": result.highest_confidence,
+        "feedback_id": result.feedback_id,
+        "classifier": "",
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+
+
+# # for local debugging:
+# if __name__ == '__main__':
+#     handler({}, {})
+# if __name__ == '__main__':
+#     with open('__events__/transcribe-collect-event.json.dist') as f:
+#         event = json.loads(f.read())
+#         handler(event, {})
