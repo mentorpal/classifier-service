14a60cd188414e3de0eb1e22ae7337c58aed5660
Milan Simonovic <mbsimonovic@gmail.com>
2022-02-11 07:49:43 +0100

    add shared folder (lfs)

---
+++ shared/spacy-model/en_core_web_sm-3.1.0/en_core_web_sm/__init__.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c8eb57079c03fc4157c227f641781fbcb3e9d11439abe1bf0b72e4c0fa78a38d
+size 237
---
+++ shared/spacy-model/en_core_web_sm-3.1.0/setup.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a84e94886cfa188e8c769622bf4720fb0763d53abe3f358c0973925512f41d6a
+size 2047
